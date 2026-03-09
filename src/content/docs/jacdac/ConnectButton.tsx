777b9da0910050ab9aa1e1b1f77f71ddc955e4a5
Thomas Ball <thomas.j.ball@outlook.com>
2026-03-09 11:39:01 +0000

    add transport plumbing

--- src/content/docs/jacdac/ConnectButton.tsx
+++ src/content/docs/jacdac/ConnectButton.tsx
@@ -1,25 +1,45 @@
 
 import React, { useState } from "react"
-import { createUSBBus, CONNECTION_STATE } from "jacdac-ts"
-import { JDBus } from "jacdac-ts"
+import { createUSBBus } from "jacdac-ts"
+import { JDBus, Transport, ConnectionState } from "jacdac-ts"
+import { useChange } from "react-jacdac"
 
 export let bus: JDBus = createUSBBus()
 bus.streaming = true
 
 export default function ConnectButton() {
-    const [connected, setConnected] = useState(false)
-    bus.on(CONNECTION_STATE, () => { 
-        setConnected(bus.connected)
-    })
+    const transport = bus.transports[0] as Transport
+    const type  = "WebUSB" // transport.type
+    const connectionState = useChange(transport, t => t.connectionState)
+    const showDisconnect =
+        connectionState == ConnectionState.Connected ||
+        connectionState == ConnectionState.Disconnecting
+    const inProgress =
+        connectionState == ConnectionState.Connecting ||
+        connectionState == ConnectionState.Disconnecting
+    const disabled =
+        connectionState != ConnectionState.Connected &&
+        connectionState != ConnectionState.Disconnected
+
+    const handleClick = showDisconnect
+        ? async () => {
+              await transport.disconnect()
+          }
+        : async () => {
+              await transport.connect()
+          }
+
+    const label = showDisconnect
+        ? `disconnect from ${type}`
+        : `connect to ${type}`
+
     return (
         <button
             style={{ marginRight: "0.5rem" }}
-            onClick={async () => {
-                await bus.disconnect()
-                await bus.connect()
-            }}
+            onClick={handleClick}
+            disabled={disabled}
         >
-            {connected ? `connected` : `press to connect`}
+            {label}
         </button>
     )
 }
